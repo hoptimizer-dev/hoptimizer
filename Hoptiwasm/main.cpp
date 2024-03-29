02938d512e9e195e205054636ba839f576ec593c
Paul Schuladen <paul.schuladen@posteo.de>
2024-03-30 00:31:56 +0100

    set qml RESOURCE_PREFIX to standard path

--- Hoptiwasm/main.cpp
+++ Hoptiwasm/main.cpp
@@ -6,13 +6,16 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/Hoptiwasm/Main.qml"_qs);
+    const QUrl url(u"qrc:/qt/qml/Hoptiwasm/Main.qml"_qs);
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
+
+    engine.addImportPath(":/qt/qml");
+
     engine.load(url);
 
     return app.exec();
